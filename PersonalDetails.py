7a34e55fcbb9f6f3209b5c51fa50e55a9ccf97a1
manisha001agrawal <67043518+manisha001agrawal@users.noreply.github.com>
2021-06-18 20:07:41 +0530

    Add files via upload

---
+++ PersonalDetails.py
@@ -0,0 +1,11 @@
+def sender_email():
+    return 'mirror010image@gmail.com'
+
+def Receiver_email():
+    return 'manisha001chandra@gmail.com'
+
+def Password():
+    return 'Swastik#123'
+
+def Whatsapp_no():
+    return "+917485078350"
